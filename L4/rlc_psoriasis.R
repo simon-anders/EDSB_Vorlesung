99626ce2c416f62510d180c80b88c97074c73a2d
Simon Anders <sanders@fs.tum.de>
2019-11-04 15:55:19 +0100

    linked charts app works now, hopefully

--- L4/rlc_psoriasis.R
+++ L4/rlc_psoriasis.R
@@ -1,16 +1,28 @@
+# Dieser R-code started die interaktive App mit den Psoriasis-Daten
+# Um die App zu starten, führen Sie einfach den gesamten untenstehenden Code in RStudio aus,
+# indem Sie auf "Source" klicken (oder indem Sie alles markieren und Ctrl-Enter drücken). 
+# Denken Sie vorher daran, mittels "Set working directory" das Arbeitsverzeichnis auf das 
+# Verzeichnis zu setzen, in dem die Dateien aus der 3. Vorlesung (counts_gene.tsv.gz, 
+# SRP035988.tsv und gene_ids.tsv) gespeichert haben.
+#
+# Vor dem ersten Ausführen müssen Sie noch R/LinkedCharts installieren, durch Eingabe
+# der beiden folgenden Befehle:
+#   install.packages( "devtools" )
+#   devtools::install_github( "anders-biostat/rlc" )
+
+
 library( tidyverse )
-devtools::install_github("anders-biostat/rlc")
 library( rlc )
 
-read_tsv( "../L3/counts_gene.tsv.gz" ) ->  countsWide
+read_tsv( "counts_gene.tsv.gz" ) ->  countsWide
 
-read_tsv( "../L3/SRP035988.tsv" ) %>%
+read_tsv( "SRP035988.tsv" ) %>%
   select( sample = run, subject = title, condition = characteristics ) %>%
   mutate( condition = fct_recode( condition,
     "normal" = "tissue type: normal skin",
     "psoriatic" = "tissue type: lesional psoriatic skin" ) ) -> sampleTable
 
-read_tsv( "../L3/gene_ids.tsv" ) -> genes
+read_tsv( "gene_ids.tsv" ) -> genes
 
 countsWide %>%
   gather( sample, count, -gene_id ) -> countsLong
@@ -67,17 +79,19 @@ lc_scatter(
     title = paste0( "Gene ", resultTable$name[i] ) ),
   logScaleY = 10,
   place = "A2",
-  ticksRotateY = 90
+  ticksRotateX = 90
 )
 
 lc_html(
   dat( content = str_glue( 
   "<h4>Gene <b>{resultTable$name[i]}</b>:</h4>", 
-  "<p>{resultTable$description[i]}</p>",
+  "<p style='width: 400px;'>{resultTable$description[i]}</p>",
   "<p>Ensembl ID: {resultTable$gene_id[i]}</p>",
   "<p><a href='http://en.wikipedia.org/wiki/{resultTable$name[i]}'>Wikipedia page on {resultTable$name[i]}</a></p>",
+  "<b>Expression summary:</b>",
   "<p><table border='1' cellpadding='6px'><tr><td>FPM</td><td>mean</td><td>std err</td></tr>",
   "<tr><td>psoriasis</td><td>{signif( resultTable$psoriatic_mean[i], 5)}</td><td>{signif( resultTable$psoriatic_sem[i], 3)}</td></tr>",
   "<tr><td>normal</td><td>{signif( resultTable$normal_mean[i], 5 )}</td><td>{signif( resultTable$normal_sem[i], 3)}</td></tr>",
   "</table></p>" ) ),
   place = "B1" )
+
